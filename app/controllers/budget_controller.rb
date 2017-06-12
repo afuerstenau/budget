62ad8eb4d0e117515fd9f1b4e1f48e8f4f4cc1d2
afuerstenau <alex@fuerstenau-clan.de>
2017-06-12 22:01:50 +0200

    removed temp files

--- app/controllers/budget_controller.rb
+++ app/controllers/budget_controller.rb
@@ -1,4 +1,33 @@
 class BudgetController < ApplicationController
+  @sums
+  
   def index
+    @activities = Activity.all
+    @categories = Category.all
+    @sums = Hash.new
+    @categories.each do |category|
+      @sums[category.id] = 0
+    end
+    @activities.each do |activity|
+      @sums[activity.category_id] += activity.amount
+    end
+  end
+  
+  def show
+    month = params[:id]
+    
+    @activities = Activity.all
+    @categories = Category.all
+    @sums = Hash.new
+    @categories.each do |category|
+      @sums[category.id] = 0
+    end
+    @activities.each do |activity|
+      if activity.months.include? month 
+        @sums[activity.category_id] += activity.amount
+      end
+    
+    end
+    render :index
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  get 'budget/index'
+  #get 'budget/index'
 
   resources :activities
   resources :categories
+  get '/budget/:id', to: 'budget#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'budget#index', as: 'budget_index'
 end
