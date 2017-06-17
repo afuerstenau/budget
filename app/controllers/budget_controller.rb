07639703c2dbcc28d298ba6df972b5381fb18793
afuerstenau <alex@fuerstenau-clan.de>
2017-06-17 17:32:25 +0200

    added incomes

--- app/controllers/budget_controller.rb
+++ app/controllers/budget_controller.rb
@@ -20,16 +20,28 @@ class BudgetController < ApplicationController
     
     @activities = Activity.all
     @categories = Category.all
-    @sums = Hash.new
+    @expenses = Hash.new
+    @incomes = Hash.new
     @categories.each do |category|
-      @sums[category.id] = 0
+      @expenses[category.id] = 0
+      @incomes[category.id] = 0
     end
+    @total_expenses = 0
+    @total_incomes = 0
     @activities.each do |activity|
       if activity.months.include? @month 
-        @sums[activity.category_id] += activity.amount
+        if activity.amount < 0
+          @expenses[activity.category_id] += activity.amount
+          @total_expenses += activity.amount
+        else
+          @incomes[activity.category_id] += activity.amount
+          @total_incomes += activity.amount
+        end
       end
-    
     end
+    
+    
+    
     render :index
   end
 end
