81ea21639b4ee35ecf2994753954bdcc6a6aeb37
afuerstenau <alex@fuerstenau-clan.de>
2017-07-10 22:14:52 +0200

    replace updated transactions

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,12 +1,20 @@
 class Transaction < ApplicationRecord
   require 'csv'
+  validates :moneymoney_id, uniqueness: true
   
   def self.import(file)
     CSV.foreach(file.path, headers: true, :col_sep => ";") do |row|
       row_as_hash = row.to_hash
       row_as_hash = replace_category_name_with_category_id (row_as_hash)
       row_as_hash.delete("category")
-      Transaction.create! row_as_hash
+      #print "moneymoney_cat #{row_as_hash['moneymoney_id']}"
+      transaction = Transaction.find_by moneymoney_id: row_as_hash['moneymoney_id']
+      #print ("found that transaction: #{transaction}")
+      if transaction == nil then
+        Transaction.create! row_as_hash
+      else
+        transaction.update row_as_hash
+      end
     end
   end
   
