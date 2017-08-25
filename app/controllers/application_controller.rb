eff1e2659d9cb933faee3e8331d73ddb36d6ae9d
afuerstenau <alex@fuerstenau-clan.de>
2017-08-25 22:38:52 +0200

    fiddeling around

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   protected
 
     def authorize
-      unless User.find_by(id: session[:user_id])
+      unless User.find_by(id: session[:user_id]) or User.take == nil
         redirect_to login_url, notice: "Please log in"
       end
     end
